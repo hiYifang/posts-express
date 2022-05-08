75269bf90c717c2e28a756c561c02d13e3c3b746
hiYifang <a6426320@gmail.com>
2022-05-08 08:33:25 +0800

    fix: 修改 app.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
 
-var app = express();
+const app = express();
 
+require('./connections/db');
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +21,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+
+// HTTP 狀態碼：404
+app.use(function (req, res, next) {
+  res.status(404).send('抱歉，您的頁面找不到');
+})
+
+// HTTP 狀態碼：500
+app.use(function (err, req, res, next) {
+  res.status(500).send('程式有些問題，請稍後嘗試');
+})
 
 module.exports = app;
