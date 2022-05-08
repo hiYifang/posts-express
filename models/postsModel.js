ad404913cacbd1998e15ee19dd859cb521e22142
hiYifang <a6426320@gmail.com>
2022-05-08 08:29:22 +0800

    feat: 新增模型

---
+++ models/postsModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// 建立 Schema
+const postsSchema = new mongoose.Schema(
+  {
+    editor: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "請填寫創作者 ID"]
+    },
+    content: {
+      type: String,
+      required: [true, '請填寫貼文內容'],
+    },
+    image: {
+      type: String,
+      default: ""
+    },
+    // 設計稿 8.我按讚的貼文
+    likes: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User"
+    },
+    createAt: {
+      type: Date,
+      default: Date.now
+    },
+  },
+  {
+    versionKey: false
+  }
+);
+
+// 建立 Model
+const Post = mongoose.model('Post', postsSchema);
+
+module.exports = Post;
---
+++ models/usersModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+// 建立 Schema
+const usersSchema = new mongoose.Schema(
+  {
+    nickName: {
+      type: String,
+      required: [true, '請填寫暱稱']
+    },
+    gender: {
+      type: Boolean,
+      default: true,
+    },
+    avatar: {
+      type: String,
+      default: ""
+    },
+    email: {
+      type: String,
+      required: [true, "請填寫 Email"],
+      unique: true,
+      lowercase: true,
+      select: false
+    },
+    password: {
+      type: String,
+      required: [true, "請填寫密碼"],
+      select: false
+    },
+    // 設計稿 4.追蹤名單
+    follower: { // 別人 -> 自己
+      type: mongoose.Schema.ObjectId,
+      ref: "User"
+    },
+    following: { // 自己 -> 別人
+      type: mongoose.Schema.ObjectId,
+      ref: "User"
+    },
+    createAt: {
+      type: Date,
+      default: Date.now
+    },
+  },
+  {
+    versionKey: false
+  }
+);
+
+// 建立 Model
+const User = mongoose.model('User', usersSchema);
+
+module.exports = User;
