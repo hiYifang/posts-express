8f7c0517bbb2bfb0a8b72fa50e0430b969cec3c3
hiYifang <a6426320@gmail.com>
2022-05-10 16:30:57 +0800

    feat: 新增貼文 API 文件

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,43 +1,98 @@
-const { successHandle, errorHandle } = require('../service/handle');
+const { getHttpResponse, getErrorMessage } = require('../service/handle');
 const Post = require('../models/postsModel');
 const User = require('../models/usersModel');
 
-module.exports = {
-  /* GET */
-  async getPosts(req, res) {
+const posts = {
+  getPosts: async (req, res) => {
+    /**
+      * #swagger.tags = ['Posts']
+      * #swagger.summary = '取得全部貼文'
+    */
+    /**
+      * #swagger.parameters['q'] = {
+        in: 'query',
+        description: '關鍵字',
+        type: 'string',
+      }
+      #swagger.parameters['sort'] = {
+        in: 'query',
+        description: '排序方式，desc 由新到舊，asc 由舊到新',
+        type: 'string',
+      }
+    */
     try {
       const { q, sort = 'desc' } = req.query;
-      console.log(req.query.sort)
       const filter = q ? { content: new RegExp(q) } : {};
-      const postData = await Post.find(filter)
+      const posts = await Post.find(filter)
         .populate({
           path: "editor",
           select: "nickName avatar"
         })
-        .sort({createdAt: sort === 'desc' ? -1 : 1});
-      successHandle(res, "取得資料成功", postData);
+        .sort({ createdAt: sort === 'desc' ? -1 : 1 });
+      /**
+        * #swagger.responses[200] = {
+          description: '成功取得資料',
+          schema: { $ref: '#/definitions/Posts' }
+        }
+      */
+      res.status(200).json(getHttpResponse({ data: posts }));
     } catch (err) {
-      errorHandle(res, err.message);
+      /**
+        * #swagger.responses[400] = {
+          description: '取得資料失敗',
+          schema: { $ref: '#/definitions/Error' }
+        }
+     */
+      res
+        .status(400)
+        .json(getHttpResponse({ success: false, data: err }));
     }
   },
-  /* Post */
-  async insertPost(req, res) {
+  insertPost: async (req, res) => {
+    /**
+     * #swagger.tags = ['Posts']
+     * #swagger.summary = '新增一則貼文'
+    */
+    /**
+     * #swagger.parameters['parameters_name'] = {
+        in: 'body',
+        description: '貼文資訊',
+        schema: {
+          $content: '貼文內容',
+          image: '貼文圖片來源'
+        }
+      }
+    */
     try {
       let { content, image } = req.body;
       if (!content) {
-        errorHandle(res, "新增失敗，請確認貼文的內容欄位");
+        throw getErrorMessage({ field: 'content', msg: '新增失敗，請確認貼文的內容欄位' });
       } else if (image && !image.startsWith('https')) {
-        errorHandle(res, "新增失敗，請確認貼文的圖片網址");
+        throw getErrorMessage({ field: 'image', msg: '新增失敗，請確認貼文的圖片網址' });
       } else {
         // 新增至 model，先固定使用者 ID
         const editorId = '627711de7054bea4d2447408';
-        await Post.create({ content, image, editor: editorId });
-        // 回傳成功
-        successHandle(res, "成功新增一則貼文", `由 ${editorId} 發文`)
+        const post = await Post.create({ content, image, editor: editorId });
+        /**
+         * #swagger.responses[200] = {
+            description: '成功新增貼文',
+            schema: { $ref: '#/definitions/Posts' }
+          }
+        */
+        res.status(200).json(getHttpResponse({ data: post }));
       }
     } catch (err) {
-      // 回傳失敗
-      errorHandle(res, err.message);
+      /**
+        #swagger.responses[400] = {
+          description: '新增貼文失敗',
+          schema: { $ref: '#/definitions/Error' }
+        }
+     */
+        res
+        .status(400)
+        .json(getHttpResponse({ success: false, data: err }));
     }
   }
 }
+
+module.exports = posts;
--- swagger.js
+++ swagger.js
@@ -17,7 +17,30 @@ const doc = {
       description: '會員'
     },
   ],
-  definitions: {}
+  definitions: {
+    Posts: {
+      data: [{
+        editor: {
+          _id: '001',
+          nickName: 'qq',
+          avatar: 'https://...'
+        },
+        content: '搶到想要的 NFT 啦',
+        image: 'https://...',
+        createdAt: '2022-05-09T08:39:25.455Z'
+      }]
+    },
+    Error: {
+      status: 'fail',
+      msg: {
+        errors: {
+          field: { // 不同欄位名稱
+            message: '錯誤訊息',
+          },
+        },
+      },
+    },
+  }
 };
 
 const outputFile = './swagger-output.json';
