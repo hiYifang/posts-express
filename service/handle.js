1270101b9c033453e81a864d9e9b34080eb55fa0
hiYifang <a6426320@gmail.com>
2022-05-10 15:41:12 +0800

    fix: 修改回傳訊息

--- service/handle.js
+++ service/handle.js
@@ -13,4 +13,16 @@ const errorHandle = (res, msg) => {
   })
 }
 
-module.exports = { successHandle, errorHandle };
+const getHttpResponse = ({ success = true, data } = {}) => {
+  const result = { status: success ? 'success' : 'fail' };
+  if (data) result[success ? 'data' : 'msg'] = data;
+  return result;
+};
+
+const getErrorMessage = ({ field, msg }) => {
+  const result = { errors: {} };
+  if (field) result.errors[field] = { msg };
+  return result;
+};
+
+module.exports = { successHandle, errorHandle, getHttpResponse, getErrorMessage };
